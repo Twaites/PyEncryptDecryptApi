7dabda8660ddf98031141ce2bdc2cb24c3c4e90c
Twaites <kory@twaites.com>
2024-06-04 20:26:38 -0700

    Adding optional key to encrypt with

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ Simple FastAPI script for encrypting and decrypting strings
 '''
 
 from cryptography.fernet import Fernet
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI(root_path="/api/v1")
 
 class EncryptMessage(BaseModel):
     message: str
+    key: str = None
 
 class DecryptKeyToken(BaseModel):
     key: str
@@ -17,18 +18,14 @@ class DecryptKeyToken(BaseModel):
 
 @app.post("/decrypt")
 def decrypt(decryptData: DecryptKeyToken):
-    try:
-        key = (decryptData.key).encode()
-        token = (decryptData.token).encode()
-        return {"message": Fernet(key).decrypt(token).decode()}
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail="Decrypt Error")
+    key = (decryptData.key).encode()
+    token = (decryptData.token).encode()
+    return {"message": Fernet(key).decrypt(token).decode()}
 
 @app.post("/encrypt")
 def encrypt(encryptData: EncryptMessage):
     try:
-        key = Fernet.generate_key()
+        key = Fernet.generate_key() if not encryptData.key else Fernet(encryptData.key)
         token = Fernet(key).encrypt(encryptData.message.encode())
         return {"key": key.decode(), "token": token.decode()}
     except Exception as e:
